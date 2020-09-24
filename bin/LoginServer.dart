60265dca2c8484ff0e8e693f10e24db75b7cf9eb
xuzhiyong <12345678>
2020-09-24 17:28:16 +0800

    提交服务端消息

--- bin/LoginServer.dart
+++ bin/LoginServer.dart
@@ -1,4 +1,4 @@
-import 'package:LoginServer/proto/helloworld.pbgrpc.dart';
+
 import 'package:grpc/grpc.dart';
 
 import 'login.dart';
@@ -6,7 +6,7 @@ import 'login.dart';
 Future<void> main(List<String> arguments) async{
   print("start server");
 
-  final server = Server([LoginServer(),GreetServer()]);
+  final server = Server([LoginServer()]);
   await server.serve(port: 50051);
   print('Server listener on port ${server.port}...');
 }
--- bin/login.dart
+++ bin/login.dart
@@ -1,6 +1,7 @@
+import 'package:LoginServer/proto/Hello.pb.dart';
 import 'package:LoginServer/proto/Hello.pbgrpc.dart';
-import 'package:LoginServer/proto/helloworld.pbgrpc.dart';
 import 'package:grpc/src/server/call.dart';
+import 'package:protobuf/protobuf.dart';
 
 class LoginServer extends LoginServerServiceBase{
   @override
@@ -8,12 +9,11 @@ class LoginServer extends LoginServerServiceBase{
     print('login info ${request.toString()}');
     return Response()..message = 'welcome ${request.username} ,your age ${request.age}';
   }
-}
 
-class GreetServer extends GreeterServiceBase{
   @override
-  Future<HelloReply> sayHello(ServiceCall call, HelloRequest request) async{
-    return HelloReply()..message = 'hello welcome';
+  Future<Response> loginout(ServiceCall call, Request request) {
+    // TODO: implement loginout
+    throw UnimplementedError();
   }
 
 }
